7c5f5180601c5ecb45a1263e02e2c81129cb480e
Bryanza Novirahman <bnrvideos@live.com>
2020-03-03 09:15:47 +0700

    Initial Commit

---
+++ Questionare/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Questionare
+//
+//  Created by Bryanza Novirahman on 03/03/20.
+//  Copyright © 2020 Bryanza Novirahman. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
