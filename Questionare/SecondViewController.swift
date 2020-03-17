7949469f01da4d6caf434e1ec17aa7077ad8f312
Bryanza Novirahman <bnrvideos@live.com>
2020-03-17 20:49:09 +0700

    No more dummy data on landing page

---
+++ Questionare/SecondViewController.swift
@@ -0,0 +1,30 @@
+//
+//  SecondViewController.swift
+//  Questionare
+//
+//  Created by Bryanza Novirahman on 12/03/20.
+//  Copyright © 2020 Bryanza Novirahman. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
